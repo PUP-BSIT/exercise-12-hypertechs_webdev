dac7daecae162ef320cb3e8863782e673f8dceee
Calib Serrano <sca@outlook.ph>
2023-11-06 20:58:17 +0800

    clean up code

--- serrano_calib/script.js
+++ serrano_calib/script.js
@@ -1,15 +1,8 @@
-function enableButton () {
-    let buttonComment = document.getElementById ("button_comment");
-    let formCommentText = document.getElementById ("form_comment_text");
-    let formCommentName = document.getElementById ("form_comment_name");
+let buttonComment = document.getElementById ("button_comment"),
+  formCommentText = document.getElementById ("form_comment_text"),
+  formCommentName = document.getElementById ("form_comment_name"); 
 
-    if (formCommentName.value.length > 0 &&
-        formCommentText.value.length > 0) {
-        buttonComment.disabled = false;
-        console.log (formCommentText.value.length);
-    } 
-    else {
-        buttonComment.disabled = true;
-    }
-    
+function enableButton () {
+  buttonComment.disabled = formCommentName.value.length < 1 || 
+    formCommentText.value.length < 1 ? true : false; 
 }
