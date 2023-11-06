ec82e17c42238ab2a797486d46f79f0e615fc6fe
Calib Serrano <sca@outlook.ph>
2023-11-06 21:31:56 +0800

    add comment adding feature

--- serrano_calib/script.js
+++ serrano_calib/script.js
@@ -1,8 +1,23 @@
-let buttonComment = document.getElementById ("button_comment"),
-  formCommentText = document.getElementById ("form_comment_text"),
-  formCommentName = document.getElementById ("form_comment_name"); 
+let buttonComment   = document.getElementById ("button_comment"),
+    formCommentText = document.getElementById ("form_comment_text"),
+    formCommentName = document.getElementById ("form_comment_name"); 
+    formComment     = document.querySelector ("#form_comment");
+
+buttonComment.addEventListener ("click", addComment);
 
 function enableButton () {
   buttonComment.disabled = formCommentName.value.length < 1 || 
     formCommentText.value.length < 1 ? true : false; 
+}
+
+function addComment () {
+  let commentName = document.createElement ("p"),
+      commentText = document.createElement ("p");
+
+  commentName.classList.add ("comment-name");
+  commentText.classList.add ("comment-text");
+  commentName.innerHTML = formCommentName.value + " said:";
+  commentText.innerHTML = formCommentText.value;
+  formComment.before (commentName);   
+  formComment.before (commentText);   
 }
