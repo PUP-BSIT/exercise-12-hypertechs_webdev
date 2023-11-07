a435b2f47944956aa302a4f89db0b6894de31e86
Calib Serrano <sca@outlook.ph>
2023-11-07 20:30:54 +0800

    correct serrano_calib/script.js

--- serrano_calib/script.js
+++ serrano_calib/script.js
@@ -1,20 +1,20 @@
-let buttonComment   = document.getElementById ("button_comment"),
-    formCommentText = document.getElementById ("form_comment_text"),
-    formCommentName = document.getElementById ("form_comment_name"); 
-    formComment     = document.querySelector ("#form_comment");
+let buttonComment = document.getElementById ("button_comment"),
+  formCommentText = document.getElementById ("form_comment_text"),
+  formCommentName = document.getElementById ("form_comment_name"), 
+  formComment     = document.querySelector ("#form_comment");
 
 buttonComment.addEventListener ("click", addComment);
 formCommentName.addEventListener ("input", enableButton);
 formCommentText.addEventListener ("input", enableButton);
 
 function enableButton () {
-  buttonComment.disabled = formCommentName.value.length < 1 || 
-    formCommentText.value.length < 1 ? true : false; 
+  buttonComment.disabled = (formCommentName.value.length < 1 || 
+    formCommentText.value.length < 1) ? true : false; 
 }
 
 function addComment () {
   let commentName = document.createElement ("p"),
-      commentText = document.createElement ("p");
+    commentText   = document.createElement ("p");
 
   commentName.classList.add ("comment-name");
   commentText.classList.add ("comment-text");
