f25bf7a268f218f89f66cd60502819d51dd9620d
mfckinstarboy <emmanuellmartinez013@gmail.com>
2023-11-07 21:29:50 +0800

    modify variable keywords

--- martinez_emmanuel/martinez-script.js
+++ martinez_emmanuel/martinez-script.js
@@ -1,27 +1,27 @@
 function addComment() {
-    var name = document.getElementById('name').value;
-    var commentText = document.getElementById('comment').value;
+    let name = document.getElementById('name').value;
+    let commentText = document.getElementById('comment').value;
 
     if (name.trim() === '' || commentText.trim() === '') {
         alert('Please fill out both the fields.');
         return;
     }
 
-    var commentList = document.getElementById('comment-list');
-    var newComment = document.createElement('div');
+    let commentList = document.getElementById('comment-list');
+    let newComment = document.createElement('div');
     newComment.classList.add('comment-container');
 
-    var userIcon = document.createElement('img');
+    let userIcon = document.createElement('img');
     userIcon.src = 'images/user_icon.jpg';
     userIcon.alt = 'User Icon';
     userIcon.className = 'user-icon';
 
-    var commentContent = document.createElement('div');
+    let commentContent = document.createElement('div');
     commentContent.classList.add('comment-content');
 
-    var nameElement = document.createElement('strong');
+    let nameElement = document.createElement('strong');
     nameElement.textContent = name;
-    var commentElement = document.createElement('p');
+    let commentElement = document.createElement('p');
     commentElement.textContent = commentText;
 
     commentContent.appendChild(nameElement);
